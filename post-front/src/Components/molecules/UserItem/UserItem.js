0066323e9839f8af49a39637348836205b1ffb83
RomeroGabriel <gabrielromerods@hotmail.com>
2020-07-17 00:40:24 -0300

    last modification

--- post-front/src/Components/molecules/UserItem/UserItem.js
+++ post-front/src/Components/molecules/UserItem/UserItem.js
@@ -2,11 +2,12 @@ import React from 'react';
 import './UserItem.css';
 
 const UserItem = ({ user }) => {
-    const { company } = user;
+    const { company, postUser } = user;
     return (
         <div className="userItem">
             <p>Nome do funcionário: {user.name}</p>
             <p>Nome da empresa: {company.name}</p>
+            <p>Nº de post {postUser.length} </p>
         </div>
     );
 };
